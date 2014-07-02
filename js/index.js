88a7b4611b2c95d45eb024afbfc208121e48f86b
David Sheffield <jeremiah.sheffield@gmail.com>
2014-07-02 04:20:09 +0000

    testing

--- js/index.js
+++ js/index.js
@@ -330,6 +330,8 @@ function showDiv(divName) {
     }
 }
 function writeNotifications() {
+    console.log('starting write notifications');
+    alert('starting write notifications');
     if (session.Storage.getItem("notifications")) {
         console.log('we have notifications');
         var notifications = JSON.parse(session.Storage.getItem("notifications"));
