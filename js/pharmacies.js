2c8dacd039dee21fbf55d195467a5f6a1b3ffe61
David Sheffield <jeremiah.sheffield@gmail.com>
2014-07-05 17:31:49 +0000

    testing

--- js/pharmacies.js
+++ js/pharmacies.js
@@ -1,6 +1,8 @@
 function getPharmaciesFromServer() {
     var url = getAppParams().server+'/pharmacys/return_json_of_pharmacies';
     $.getJSON( url, function( data ) {
-      alert(data);
+        for (var x=0;x<data.pharmacies.length;x++) {
+           $('#refill_pharmacy').append('<option>'+data.pharmacies[x]+'</option>');
+        }
     });
 }
